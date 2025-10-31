17e6cacc15e723bae915a467e3e70e93c1e30919
StevenSopilidis <sopilidimaria@gmail.com>
2025-10-31 09:21:03 +0200

    implemented domain for ordering service

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,22 @@
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+    {
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] dequeuedEvents = [ .. _domainEvents];
+            _domainEvents.Clear();
+            return dequeuedEvents;
+        }
+    }
+}
